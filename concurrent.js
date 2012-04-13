906b4b149dd1e4f955dc4c872bc2830d36486466
Chetan Surpur <chetan51@gmail.com>
2012-04-12 21:50:52 -0700

    Bit o' progress

--- concurrent.js
+++ concurrent.js
@@ -1,19 +1,31 @@
-if (Meteor.is_client) {
-  Template.hello.greeting = function () {
-    return "Welcome to concurrent.";
-  };
+Messages = new Meteor.Collection("messages");
 
-  Template.hello.events = {
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
+if (Meteor.is_client) {
+  Template.chat.messages = function() {
+    return Messages.find();
   };
+  
+  Template.message.complete = function() {
+    return this.complete ? " complete" : '';
+  }
+  
+  Template.message.editing = function() {
+    return true;
+  }
+
+  //Template.hello.events = {
+    //'click input' : function () {
+      //// template data, if any, is available in 'this'
+      //if (typeof console !== 'undefined')
+        //console.log("You pressed the button");
+    //}
+  //};
 }
 
 if (Meteor.is_server) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    if (Messages.find().count() === 0) {
+      Messages.insert({name: "concurrent", text: "welcome!", complete: true});
+    }
   });
 }
